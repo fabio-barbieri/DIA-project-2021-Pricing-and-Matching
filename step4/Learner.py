fee0293a5534e01bb1799fb9c2b55cf6641052c3
mussigiuli <75251110+mussigiuli@users.noreply.github.com>
2021-07-02 17:00:22 +0200

    add self.expected_customers

--- step4/Learner.py
+++ step4/Learner.py
@@ -9,16 +9,18 @@ class Learner:
         self.rewards_per_arm = [[] for _ in range(n_arms)]
         self.collected_rewards = []
         self.m = np.array([config.TOT_CUSTOMERS // 4 for _ in range(4)]) # Non-informative prior
-        self.s_2 = np.ones((4, 1))
+        self.s_2 = np.array([1.0, 1.0, 1.0, 1.0])
+        self.expected_customers = np.random.normal(self.m, self.s_2, size = (4,1)) #initial number of expected customers per class, according to our prior
 
     def update_observations(self, pulled_arm, reward):
         self.rewards_per_arm[pulled_arm].append(reward)  # ... (for UCB ???)
         self.collected_rewards.append(reward)  # List of N_EXPS arrays of shape (T, 4)
 
-    def compute_posterior(x_bar):
+    def compute_posterior(self, x_bar):
         sigma_2 = config.SD_CUSTOMERS ** 2
         m = self.m
         s_2 = self.s_2
 
         self.m = (s_2 * x_bar + m * sigma_2) / (s_2 + sigma_2)
         self.s_2 = (s_2 * sigma_2) / (s_2 + sigma_2)
+        self.expected_customers = np.random.normal(self.m, self.s_2, size = (4,1))
