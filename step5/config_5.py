2fe84c19cd8d3a8374f521e7b04b29b7f614ed7c
mussigiuli <75251110+mussigiuli@users.noreply.github.com>
2021-07-07 16:12:18 +0200

    Update config_5.py

--- step5/config_5.py
+++ step5/config_5.py
@@ -1,9 +1,8 @@
 import numpy as np
 import utils_5
 
-T = 1  # Time horizon
-N_EXPS = 1  # Number of experiments
-N_ARMS = 5  # Number of different candidate prices
+T = 365  # Time horizon
+N_EXPS = 100  # Number of experiments
 NUM_CUSTOMERS = np.array([20, 40, 10, 30])  # Mean of the number of total daily customers per class
 SD_CUSTOMERS = np.array([2, 4, 1, 3])  # standard deviation on the number of customers per each class
 TOT_CUSTOMERS = np.sum(NUM_CUSTOMERS)
