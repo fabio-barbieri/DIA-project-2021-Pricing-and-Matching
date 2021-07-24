fixed opt computation in step6

step6/config_6.py
@@ -3,10 +3,10 @@ import utils_6
 from hungarian_algorithm_6 import hungarian_algorithm
 
 T = 365  # Time horizon
-N_EXPS = 10  # Number of experiments
+N_EXPS = 1  # Number of experiments
 
 N_ARMS_1 = 5  # Number of different candidate prices
-N_ARMS_2 = 10  # Number of candidates for second item price
+N_ARMS_2 = 5  # Number of candidates for second item price
 
 NUM_CUSTOMERS = np.array([20, 40, 10, 30])  # Mean of the number of total daily customers per class
 SD_CUSTOMERS = np.array([2, 4, 1, 3])  # standard deviation on the number of customers per each class
@@ -34,6 +34,9 @@ for margin in MARGINS_2:
         tmp.append(cr)
     CR2.append(tmp)
 
+CR1 = np.array(CR1)
+CR2 = np.array(CR2)
+
 def compute_opt_matching():
         opt_value = -1
         for arm_1 in range(N_ARMS_1):  # For every price_1
@@ -48,11 +51,11 @@ def compute_opt_matching():
 
         return opt_value  #, (idx1, idx2)
 
-def build_matrix(self, idx1, idx2): 
+def build_matrix(idx1, idx2): 
         n_promos = (PROMO_PROB[1 :] * TOT_CUSTOMERS).astype(int)
         n_promos = np.insert(n_promos, 0, TOT_CUSTOMERS - np.sum(n_promos))
 
-        profit =   CR1 * (MARGINS_1[idx1] + CR2 * MARGINS_2[idx2])
+        profit =   CR1[idx1] * (MARGINS_1[idx1] + CR2[idx2] * MARGINS_2[idx2])
 
         # repeat columns
         matrix = np.repeat(profit, n_promos, axis=1)
@@ -63,4 +66,4 @@ def build_matrix(self, idx1, idx2):
         return matrix
 
 
-OPT = compute_opt_matching
+OPT = compute_opt_matching()