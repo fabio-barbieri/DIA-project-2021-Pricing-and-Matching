82742c50fc1decb35e491be80f2d289647a8925b
mussigiuli <75251110+mussigiuli@users.noreply.github.com>
2021-08-16 11:08:35 +0200

    update promo discounts 5

--- step5/config_5.py
+++ step5/config_5.py
@@ -83,8 +83,9 @@ def compute_cr1(price, cl):
 
 CR1 = np.array([compute_cr1(MARGIN_1, c) for c, _ in enumerate(NUM_CUSTOMERS)])
 
-MARGINS_2 = np.array([29.99, 24.99, 20.99, 10.99])
-                      # p0     p1     p2     p3
+PROMO_DISCOUNTS = np.array([1, 0.85, 0.75, 0.60])
+                         # p0   p1    p2    p3
+MARGINS_2 = (29.99 * PROMO_DISCOUNTS).reshape((1,4))
 
 CR2 = np.array([[0.2, 0.4, 0.3, 0.3],  # Junior Professionals
                 [0.0, 0.2, 0.3, 0.5],  # Junior Amateur
